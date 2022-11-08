cd7d4280203e0b0113bdb40926ba6e896c142015
iseau395 <70857830+iseau395@users.noreply.github.com>
2022-11-08 06:23:24 -0800

    Update deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,19 +1,27 @@
 // @ts-check
 import ghpages from'gh-pages';
 
+const url = "https://github.com/iseau395/squash-shootout";
+
+console.log(`Pushing to the gh-pages branch on ${url} in 5 seconds...`)
+
 let errored = false;
-
-ghpages.publish('public', 
-    {
-        branch: 'gh-pages',
-        repo: 'https://github.com/iseau395/among-us-clicker',
-        message: `Automatically update github pages branch`,
+setTimeout(
+    () => {
+        ghpages.publish('public', 
+            {
+                branch: 'gh-pages',
+                repo: url,
+                message: `Automatically update github pages branch`,
+            },
+            (err) => {
+                errored = true;
+                if (err) console.error(err);
+            }
+        );
+    
+        if (!errored)
+            console.log("Successfully Published!");
     },
-    (err) => {
-        errored = true;
-        if (err) console.error(err);
-    }
-);
-
-if (!errored)
-    console.log("Successfully Published!");
+    5000
+)
